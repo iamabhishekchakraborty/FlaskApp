a06ac6cabec6e047661b32555ef6fb21eaa60dd0
abhishek-ces <abhishek.chakraborthy@cesltd.com>
2020-12-31 21:36:05 +0530

    jenkinsfile changes

--- scripts/deploy-docker-heroku.sh
+++ scripts/deploy-docker-heroku.sh
@@ -5,6 +5,7 @@ set -e
 imagename=$1
 appname=$2
 echo "Setting up to deploy docker image "$imagename" to site servers (Heroku) "$appname""
+heroku container:login
 docker tag "$imagename" registry.heroku.com/"$appname"/web
 docker push registry.heroku.com/"$appname"/web
 heroku container:release web --app "$appname"
