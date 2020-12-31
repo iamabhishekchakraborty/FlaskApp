c03f246487e3e98b2d97cfb51c848ba099e60d5d
abhishek-ces <abhishek.chakraborthy@cesltd.com>
2020-12-31 16:47:44 +0530

    shell script mode change

--- scripts/deploy-docker-heroku.sh
+++ scripts/deploy-docker-heroku.sh
@@ -5,6 +5,6 @@ set -e
 imagename=$1
 appname=$2
 echo "Setting up to deploy docker image "$imagename" to site servers (Heroku) "$appname""
-docker tag "$imagename" registry.heroku.com/<app>/web
-docker push registry.heroku.com/<app>/web
+docker tag "$imagename" registry.heroku.com/"$appname"/web
+docker push registry.heroku.com/"$appname"/web
 heroku container:release web --app "$appname"
